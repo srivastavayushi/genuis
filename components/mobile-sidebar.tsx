3601766d22e6b1203ff5406760bbd6e6a0697061
Ayushi <ayushimanoj1201@gmail.com>
2023-11-27 11:59:18 +0530

    remove sidebar component hydration error

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -9,8 +9,16 @@ import {
   SheetTrigger,
 } from "./ui/sheet";
 import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
   return (
     <Sheet>
       <SheetTrigger>
