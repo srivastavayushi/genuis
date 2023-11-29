f2525068bf0dee0434a522e6d9dff490de76d2a2
Ayushi <ayushimanoj1201@gmail.com>
2023-11-29 10:19:33 +0530

    add code generation API + style

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,10 +1,17 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const instructionMessage: ChatCompletionMessageParam = {
+  role: "system",
+  content:
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+};
 
 export async function POST(req: Request) {
   try {
@@ -32,12 +39,12 @@ export async function POST(req: Request) {
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
-      messages,
+      messages: [instructionMessage, ...messages],
     });
 
     return NextResponse.json(response.choices[0].message);
   } catch (error) {
-    console.log("[CONVERSATION_ERROR", error);
+    console.log("[CODE_ERROR", error);
     return new NextResponse("Internal Error", {
       status: 500,
     });
